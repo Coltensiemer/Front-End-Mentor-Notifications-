10ad124b00488734edc1e6d0720a32688c3b94e3
Colten Siemer <coltensiemer@Coltens-MacBook-Pro.local>
2022-12-04 11:40:13 -0500

    Work on JS for click events for red dots

--- Plus_Jakarta_Sans/js.js
+++ Plus_Jakarta_Sans/js.js
@@ -5,24 +5,22 @@
     const profile = document.querySelectorAll(".profile-container"); 
     const removeAllBtn = document.querySelector('.remove-all-btn');
     const dot= document.querySelectorAll(".span-dot") ;
-    const divDot = document.getElementById('div-dot')
     
     
     
     
-    // divDot.classList.add('hidden')// does not work 
+    
+    
     
     // When you click on profile 
     
     profile.forEach(profiles => { 
         profiles.addEventListener('click', ()=> { 
-          profiles.classList.remove('active');
-          
+          profiles.classList.remove('active')
         })
-        dot.addEventListener('click', ()=> { 
-            dot.classList.add('hidden')
-        })
-    })
+        }); 
+    
+        
     
     //Mark all as read 
     
