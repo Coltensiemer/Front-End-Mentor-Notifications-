43c8c9578e4500390a5155c484be1d6a5072a3ef
Colten Siemer <coltensiemer@Coltens-MacBook-Pro.local>
2022-12-04 09:17:13 -0500

    Transfer over files to recreate REPO

---
+++ js.js
@@ -0,0 +1,36 @@
+'use strict'
+
+
+ // const profileElement = document.getElementById("profile-element"); 
+    const profile = document.querySelectorAll(".profile-container"); 
+    const removeAllBtn = document.querySelector('.remove-all-btn');
+    const dot= document.querySelectorAll(".span-dot") ;
+    
+    
+    
+    
+    
+    
+    
+    // When you click on profile 
+    
+    profile.forEach(profiles => { 
+        profiles.addEventListener('click', ()=> { 
+          profiles.classList.remove('active')
+        })
+        }); 
+    
+        
+    
+    //Mark all as read 
+    
+    removeAllBtn.addEventListener('click', ()=> { 
+        profile.forEach(profiles => { 
+            profiles.classList.remove('active')
+           
+        })
+    })
+
+
+
+
